d2ce0ab218443282daf02ed25f0a28cd6ccde937
Matheus Chaves <matheusca@hotmail.com>
2022-07-04 17:26:27 -0300

    component registration

---
+++ src/main/java/com/chaves/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.chaves.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chaves.course.entities.User;
+import com.chaves.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
