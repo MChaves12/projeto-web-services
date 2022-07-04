94e9e209ece071746188687926792052f5b2611d
Matheus Chaves <matheusca@hotmail.com>
2022-07-04 10:01:36 -0300

    Project Created

---
+++ src/test/java/com/chaves/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaves.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
